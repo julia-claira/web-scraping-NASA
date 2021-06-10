80ff9feeb38c0d72696b46891b3ae2b73ef94bb8
Claira J <julia.claira@gmail.com>
2021-06-10 10:15:27 -0700

    created flask app

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,43 +1,27 @@
 from flask import Flask, render_template, redirect
-from flask pymongo import PyMongo
-import scrape_mars
+from flask_pymongo import PyMongo
+from scrape_mars import scrape
 
 app = Flask(__name__)
 
 #set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/phone_app"
-MONGO = PyMongo(app)
-
-@app.route("/")
-
-
-from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-import scrape_phone
-
-app = Flask(__name__)
-
-# Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/phone_app"
 mongo = PyMongo(app)
 
-# Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/phone_app")
-
-
-@app.route("/")
-def index():
-    listings = mongo.db.listings.find_one()
-    return render_template("index.html", listings=listings)
-
-
+#runs scrape function and updates db with the dictionary
 @app.route("/scrape")
 def scraper():
-    listings = mongo.db.listings
-    listings_data = scrape_phone.scrape()
-    listings.update({}, listings_data, upsert=True)
+    facts = mongo.db.facts
+    facts_data = scrape()
+    facts.update({}, facts_data, upsert=True)
     return redirect("/", code=302)
+    
 
+#queries mongo db and passes results into html   
+@app.route("/")
+def index():
+    facts = mongo.db.facts.find_one()
+    return render_template("index.html", facts=facts)
 
 if __name__ == "__main__":
     app.run(debug=True)
